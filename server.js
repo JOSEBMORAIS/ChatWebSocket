650e0840f11798edc9d30eeda24f00380da16d21
Jose Benedito de Morais <79100375+JOSEBMORAIS@users.noreply.github.com>
2025-04-15 09:18:38 -0300

    Update server.js
    Colocar a porta listen em variavel

--- server.js
+++ server.js
@@ -60,7 +60,7 @@ wss.on('connection', function connection(ws) {
   ws.send(JSON.stringify({ tipo: 'mensagem', texto: 'Bem-vindo ao chat!' }));
 });
 
-server.listen(process.env.PORT || 3000, () => {
-  console.log('Servidor rodando na porta', process.env.PORT || 3000);
+server.listen(process.env.PORT || port, () => {
+  console.log('Servidor rodando na porta', process.env.PORT || ${port});
 });
 
